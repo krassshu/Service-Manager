0f0944a4cedd75991b21dc142f890ac4a3a3abb8
Oskar Bohuszewicz-Sluszniak <oskarsluszniak@gmail.com>
2024-08-28 15:33:09 +0200

    commented img atribute and added working div

--- src/components/Protocols/Protocols.tsx
+++ src/components/Protocols/Protocols.tsx
@@ -1,13 +1,12 @@
 import Image from "next/image";
-import Link from "next/link";
 
 
 export default function Protocols(){
 
     return(
             <a href="#" target="_blank" className="p-2 max-w-28 flex flex-col  rounded-lg shadow-lg bg-white border-e border-gray-200 lg:block lg:translate-x-0 lg:end-auto lg:bottom-0  dark:bg-slate-800 dark:border-neutral-700 transition-transform duration-200 hover:scale-95">
-                <div className="bg-amber-50">
-                    <Image width={24} height={24} src={"/"} alt={""}/>
+                <div className="bg-amber-50 w-full h-[100px] ">
+                    {/*<Image width={24} height={24} src={"/"} alt={""}/>*/}
                 </div>
                 <div>
                     <span className="text-sm font-semibold leading-6 text-gray-900 dark:text-neutral-200">Title</span>
